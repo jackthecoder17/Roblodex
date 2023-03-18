cf19b2d4a4685e7846003b712fc054c5f378a55d
= <=>
2023-03-18 20:19:03 +0100

    roblodex done

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,15 @@
+
+
+ import "./search-box.styles.css"
+
+ const SearchBox = ({className, placeholder,onChangeHandler})=>(
+
+    <input 
+    type="search"  
+    placeholder={placeholder}
+    className={` search-box ${className}`}
+    onChange={ onChangeHandler}
+    />
+        
+ )     
+ export default SearchBox
